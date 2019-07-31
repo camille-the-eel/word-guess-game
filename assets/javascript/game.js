7b052a17dd7533d7b858efb4edcb33d2a2973d4d
Camille <c.hughes64@gmail.com>
2019-07-31 11:22:59 -0400

    success: no repeat guesses //bug: for loop

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,15 +25,15 @@ var wordChoices = [
 ];
 var wins = 0;
 var losses = 0;
-var startingGuesses = 15;
-var remainingGuesses = startingGuesses;
+var remainingGuesses = 15;
+var userGuessed = [];
+var lettersGuessed = [];
 
 //VARIABLES FOR LINKING JS TO HTML CLASSES
 var winsText = document.querySelector(".wins");
 var lossesText = document.querySelector(".losses");
 var remainingGuessesText = document.querySelector(".guesses");
-var lettersGuessed = document.querySelector(".letters-guessed");
-var userGuessed = [""];
+var lettersGuessedText = document.querySelector(".letters-guessed");
 var blankText = document.querySelector(".word-blanks");
 
 //FUNCTIONS
@@ -60,7 +60,7 @@ function gameSet () {
 
     //RESETS THE USER GUESSED LETTERS
     userGuessed = [];
-    lettersGuessed.textContent = userGuessed;
+    lettersGuessed = [];
 }
 
 
@@ -96,27 +96,37 @@ document.onkeyup = function(event) {
         //IF/ELSE STATEMENT TO ONLY ALLOW LETTERS AS GUESSES
         if (userGuess === "a" || userGuess === "b" || userGuess === "c" || userGuess === "d" || userGuess === "e" || userGuess === "f" || userGuess === "g" || userGuess === "h" || userGuess === "i" || userGuess === "j" || userGuess === "k" || userGuess === "l" || userGuess === "m" || userGuess === "n" || userGuess === "o" || userGuess === "p" || userGuess === "q" || userGuess === "r" || userGuess === "s" || userGuess === "t" || userGuess === "u" || userGuess === "v" || userGuess === "w" || userGuess === "x" || userGuess === "y" || userGuess === "z") {
             
-            userGuessed.push(" " + userGuess);
-            remainingGuesses--;
-            remainingGuessesText.textContent = remainingGuesses;
+            let g = userGuessed.indexOf(userGuess);
+        
+            if (g === -1) {
+                userGuessed.push(userGuess);
+                lettersGuessed.push(" " + userGuess);
+
+                remainingGuesses--;
+                remainingGuessesText.textContent = remainingGuesses;
+
+                //FOR LOOP PLUS IF/ELSE TO COMPARE LETTER GUESSES WITH THE INDEX OF EACH CHARACTER IN THE COMPUTER SELECTED WORD
+                for (var j =0; j < computerSelect.length; j++) {
+                    if (computerSelect[j] === userGuess) {
+                        answerSpaces[j] = userGuess;
+                        remainingLetters--;
+                        console.log(answerSpaces.join(" "));
+                        blankText.textContent = answerSpaces.join(" ");
+                        console.log("RIGHT");
+                    }
+                    else if (computerSelect[j] !== userGuess) { 
+                        lettersGuessedText.textContent = lettersGuessed;
+                        console.log("WRONG");
+                    }
+                }
+            } else {
+                console.log("...you've already guessed that. Try a different letter.");
+            }
 
             gameOutcome();
         }
         else {
             alert("Press a letter, smartass.")
-        }
-
-        //FOR LOOP PLUS IF/ELSE TO COMPARE LETTER GUESSES WITH THE INDEX OF EACH CHARACTER IN THE COMPUTER SELECTED WORD
-        for (var j =0; j < computerSelect.length; j++) {
-            if (computerSelect[j] === userGuess) {
-                answerSpaces[j] = userGuess;
-                remainingLetters--;
-                console.log(answerSpaces.join(" "));
-                blankText.textContent = answerSpaces.join(" ");
-            }
-            else if (computerSelect[j] !== userGuess) { 
-                lettersGuessed.textContent = userGuessed;
-            }
         }
 }
 
