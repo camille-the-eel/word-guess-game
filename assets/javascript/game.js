0461ecfb2b7bdb172a9b0471f88338b016674954
Camille <c.hughes64@gmail.com>
2019-07-31 15:23:57 -0400

    indicator of correct word guess and reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
-
 //BASE VARIABLES
 //===========================================
 
@@ -28,13 +27,15 @@ var losses = 0;
 var remainingGuesses = 15;
 var userGuessed = [];
 var lettersGuessed = [];
+var wordSuccess = false;
 
-//VARIABLES FOR LINKING JS TO HTML CLASSES
+//LINKING JS TO HTML CLASSES
 var winsText = document.querySelector(".wins");
 var lossesText = document.querySelector(".losses");
 var remainingGuessesText = document.querySelector(".guesses");
 var lettersGuessedText = document.querySelector(".letters-guessed");
 var blankText = document.querySelector(".word-blanks");
+
 
 //FUNCTIONS
 //=============================================
@@ -61,12 +62,14 @@ function gameSet () {
     //RESETS THE USER GUESSED LETTERS
     userGuessed = [];
     lettersGuessed = [];
+
+    //RESETS CORRECT GUESS
+    wordSuccess = false;
 }
 
 
 //GAME PLAY
-//============================================
-
+//scoring -- needs work
 function gameOutcome () {
 
     if (remainingGuesses === 0) {
@@ -83,51 +86,56 @@ function gameOutcome () {
 };
 
 
-//INSIDE ON KEY UP
-//=============================================
-
 //RUNS WHENEVER PLAYER PRESSES A KEY
 document.onkeyup = function(event) {
+
+    wordSuccess = false;
 
     //DETERMINES WHICH KEY WAS PRESSED
     var userGuess = event.key;
     console.log(userGuess);
 
-        //IF/ELSE STATEMENT TO ONLY ALLOW LETTERS AS GUESSES
-        if (userGuess === "a" || userGuess === "b" || userGuess === "c" || userGuess === "d" || userGuess === "e" || userGuess === "f" || userGuess === "g" || userGuess === "h" || userGuess === "i" || userGuess === "j" || userGuess === "k" || userGuess === "l" || userGuess === "m" || userGuess === "n" || userGuess === "o" || userGuess === "p" || userGuess === "q" || userGuess === "r" || userGuess === "s" || userGuess === "t" || userGuess === "u" || userGuess === "v" || userGuess === "w" || userGuess === "x" || userGuess === "y" || userGuess === "z") {
-            
-            let g = userGuessed.indexOf(userGuess);
+    //IF/ELSE STATEMENT TO ONLY ALLOW LETTERS AS GUESSES
+    if (userGuess === "a" || userGuess === "b" || userGuess === "c" || userGuess === "d" || userGuess === "e" || userGuess === "f" || userGuess === "g" || userGuess === "h" || userGuess === "i" || userGuess === "j" || userGuess === "k" || userGuess === "l" || userGuess === "m" || userGuess === "n" || userGuess === "o" || userGuess === "p" || userGuess === "q" || userGuess === "r" || userGuess === "s" || userGuess === "t" || userGuess === "u" || userGuess === "v" || userGuess === "w" || userGuess === "x" || userGuess === "y" || userGuess === "z") {
         
-            if (g === -1) {
-                userGuessed.push(userGuess);
-                lettersGuessed.push(" " + userGuess);
+        let g = userGuessed.indexOf(userGuess);
+    
+        if (g === -1) {
+            userGuessed.push(userGuess);
+            lettersGuessed.push(" " + userGuess);
 
-                remainingGuesses--;
-                remainingGuessesText.textContent = remainingGuesses;
+            remainingGuesses--;
+            remainingGuessesText.textContent = remainingGuesses;
 
-                //FOR LOOP PLUS IF/ELSE TO COMPARE LETTER GUESSES WITH THE INDEX OF EACH CHARACTER IN THE COMPUTER SELECTED WORD
-                for (var j =0; j < computerSelect.length; j++) {
-                    if (computerSelect[j] === userGuess) {
-                        answerSpaces[j] = userGuess;
-                        remainingLetters--;
-                        console.log(answerSpaces.join(" "));
-                        blankText.textContent = answerSpaces.join(" ");
-                        console.log("RIGHT");
-                    }
-                    else if (computerSelect[j] !== userGuess) { 
-                        lettersGuessedText.textContent = lettersGuessed;
-                        console.log("WRONG");
-                    }
-                }
-            } else {
-                console.log("...you've already guessed that. Try a different letter.");
-            }
+            wordCheck(userGuess);
+            console.log("SUCCESS", wordSuccess);
+        } else {
+            alert("...you've already guessed that. Try a different letter.");
+        }
 
-            gameOutcome();
+        gameOutcome();
+
+    }
+    else {
+        alert("Press a letter, smartass.")
+    }
+}
+
+function wordCheck(userGuess) {
+
+    //FOR LOOP PLUS IF/ELSE TO COMPARE LETTER GUESSES WITH THE INDEX OF EACH CHARACTER IN THE COMPUTER SELECTED WORD
+    for (var j =0; j < computerSelect.length; j++) {
+        if (computerSelect[j] === userGuess) {
+            answerSpaces[j] = userGuess;
+            remainingLetters--;
+            console.log(answerSpaces.join(" "));
+            blankText.textContent = answerSpaces.join(" ");
+            wordSuccess = true;
         }
         else {
-            alert("Press a letter, smartass.")
+            lettersGuessedText.textContent = lettersGuessed;
         }
+    }
 }
 
 gameSet();
