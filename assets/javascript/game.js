e2affc82430b17f6da3b02cbb073c308883f7802
Camille <c.hughes64@gmail.com>
2019-07-31 16:07:25 -0400

    scoring corrected // game logic complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -50,7 +50,6 @@ function gameSet () {
         answerSpaces[i]="_";
     }
     remainingLetters = computerSelect.length;
-    
 
     //RESETS BLANKS
     blankText.textContent = answerSpaces.join(" ");
@@ -63,32 +62,14 @@ function gameSet () {
     userGuessed = [];
     lettersGuessed = [];
 
-    //RESETS CORRECT GUESS
+    //RESETS CORRECT GUESS INDICATOR
     wordSuccess = false;
 }
-
-
-//GAME PLAY
-//scoring -- needs work
-function gameOutcome () {
-
-    if (remainingGuesses === 0) {
-        alert("You Lose");
-        losses++;
-        lossesText.textContent = losses;
-        gameSet();
-    } else if (remainingLetters === -1 ) {
-        alert("You Win!");
-        wins++;
-        winsText.textContent = wins;
-        gameSet();
-    }
-};
-
 
 //RUNS WHENEVER PLAYER PRESSES A KEY
 document.onkeyup = function(event) {
 
+    //RESETS CORRECT GUESS INDICATOR
     wordSuccess = false;
 
     //DETERMINES WHICH KEY WAS PRESSED
@@ -98,9 +79,10 @@ document.onkeyup = function(event) {
     //IF/ELSE STATEMENT TO ONLY ALLOW LETTERS AS GUESSES
     if (userGuess === "a" || userGuess === "b" || userGuess === "c" || userGuess === "d" || userGuess === "e" || userGuess === "f" || userGuess === "g" || userGuess === "h" || userGuess === "i" || userGuess === "j" || userGuess === "k" || userGuess === "l" || userGuess === "m" || userGuess === "n" || userGuess === "o" || userGuess === "p" || userGuess === "q" || userGuess === "r" || userGuess === "s" || userGuess === "t" || userGuess === "u" || userGuess === "v" || userGuess === "w" || userGuess === "x" || userGuess === "y" || userGuess === "z") {
         
+        //DETERMINES IF USER HAS ALREADY MADE CURRENT GUESS
         let g = userGuessed.indexOf(userGuess);
-    
         if (g === -1) {
+            //IF NOT: ADD TO ARRAY OF GUESSES
             userGuessed.push(userGuess);
             lettersGuessed.push(" " + userGuess);
 
@@ -113,7 +95,8 @@ document.onkeyup = function(event) {
             alert("...you've already guessed that. Try a different letter.");
         }
 
-        gameOutcome();
+        //CHECKING FOR WIN/LOSS
+        setTimeout(checkOutcome, 300)
 
     }
     else {
@@ -121,21 +104,43 @@ document.onkeyup = function(event) {
     }
 }
 
+//CHECKING EACH GUESS
 function wordCheck(userGuess) {
 
-    //FOR LOOP PLUS IF/ELSE TO COMPARE LETTER GUESSES WITH THE INDEX OF EACH CHARACTER IN THE COMPUTER SELECTED WORD
+    //FOR LOOP PLUS IF/ELSE TO COMPARE LETTER GUESSES WITH THE INDEX OF EACH CHARACTER IN THE COMPUTER SELECTED 
+    
+    //THIS CHECKS EVERY INDEX, SO THE IF/ELSE RUNS FOR EACH BLANK SPACE. THEREFORE THE ELSE WILL FIRE EVERY GUESS 
+    //(AS THERE IS NO WORD THAT HAS ONE LETTER AS EVERY BLANK)--ENSURING THE CURRENT userGuess(LETTER) IS ADDED TO THE GUESSED ARRAY
     for (var j =0; j < computerSelect.length; j++) {
         if (computerSelect[j] === userGuess) {
             answerSpaces[j] = userGuess;
-            remainingLetters--;
             console.log(answerSpaces.join(" "));
             blankText.textContent = answerSpaces.join(" ");
             wordSuccess = true;
+            remainingLetters--;
         }
         else {
             lettersGuessedText.textContent = lettersGuessed;
         }
     }
+
 }
 
+//CHECKING FOR WIN OR LOSS
+function checkOutcome () {
+
+    if (wordSuccess === true && remainingLetters === 0) {
+        alert("You Win!");
+        wins++;
+        winsText.textContent = wins;
+        gameSet();
+    } else if (remainingGuesses === 0) {
+        alert("You Lose");
+        losses++;
+        lossesText.textContent = losses;
+        gameSet();
+    }
+};
+
+//SETS THE GAME ON PAGE LOAD
 gameSet();
